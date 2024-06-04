a5462ae8a27d38e06f6ce881bbabbb7c267b3213
Weverthon-Miguel <136903060+Weverthon-Miguel@users.noreply.github.com>
2024-06-04 20:13:17 -0300

    .

--- aula 9/index.php
+++ aula 9/index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário de login</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="style1.css">
 </head>
 <body>
+    <div id="a">
     <h1>Wish List</h1>
     <div>
         <h2>
@@ -26,6 +27,7 @@
                 <button type="reset">Limpar</button>
             </div>
         </form>  
+    </div>
     </div>
 </body>
 </html>
